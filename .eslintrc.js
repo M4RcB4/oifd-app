0e2ed594d404d4f0a97c51590a9542d32f3d4a9f
Marcus Bates <marcus.a.bates@gmail.com>
2020-11-08 16:22:15 -0700

    Fixed issues with ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
-    extends: ['react-app', 'prettier', 'cypress'],
-    plugins: ['prettier', 'jest'],
-    parser: 'babel-eslint',
-    env: {
-      browser: true,
-      es6: true,
-      'jest/globals': true,
+  extends: ["react-app", "prettier"],
+  plugins: ["prettier", "jest"],
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    "jest/globals": true,
+  },
+  settings: {
+    react: {
+      version: "999.999.999",
     },
-    settings: {
-      react: {
-        version: 'latest',
-      },
-    },
-    rules: {
-      'prettier/prettier': 'warn',
-      'cypress/no-assigning-return-values': 'error',
-      'cypress/no-unnecessary-waiting': 'error',
-      'cypress/assertion-before-screenshot': 'warn',
-      'cypress/no-force': 'warn',
-      'cypress/no-async-tests': 'error'
-    },
-  };
+  },
+  rules: {
+    "prettier/prettier": "warn",
+  },
+};
