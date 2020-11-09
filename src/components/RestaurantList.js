6cf177bed2aa3f61b28f80f2b76ecc822632c189
Marcus Bates <marcus.a.bates@gmail.com>
2020-11-08 18:52:13 -0700

    The unit tests are now passing

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -5,12 +5,18 @@ In addition to the default export, we also do a named export of the
 component. This is because later our default export will be the RestaurantList 
 connected to Redux, but we will also want access to the unconnected component 
 for testing. */
-export const RestaurantList = ({ loadRestaurants }) => {
+export const RestaurantList = ({ loadRestaurants, restaurants }) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
-  return <div>RestaurantList</div>;
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>{restaurant.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default RestaurantList;
--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -7,9 +7,30 @@ describe("RestaurantList", () => {
   it("loads restaurants on first render", () => {
     // Since this is a unit test we don't want to connect to the redux store
     const loadRestaurants = jest.fn().mockName("loadRestaurants");
+    const restaurants = [];
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it("displays the restaurants", () => {
+    const noop = () => {};
+    const restaurants = [
+      { id: 1, name: "Sushi Place" },
+      { id: 2, name: "Pizza Place" },
+    ];
+
+    const { queryByText } = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText("Sushi Place")).not.toBeNull();
+    expect(queryByText("Pizza Place")).not.toBeNull();
   });
 });
